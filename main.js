939cc4708d476f227b371678d7cdea3563775843
Le Huynh <lhuynh100@gmail.com>
2022-09-29 15:38:22 -0500

    commit message

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphObject = {'♡':FULL_HEART,'♥':EMPTY_HEART}
+const colorState = {"red":"","":"red"}
 
+let likeButtons = document.querySelectorAll(".like-glyph")
+
+for (let el of likeButtons) {
+  
+  el.addEventListener("click", (e) => {
+    mimicServerCall()
+    .then((res) => { 
+     const heart = e.target;
+     if ( heart.className === "activated-heart" )
+     {
+      heart.className.remove() 
+      heart.textContent ="♡"
+     } else{
+      heart.textContent = "♥"
+      heart.className = "activated-heart"
+     }
+    })
+
+    .catch((error) => {document.getElementById("modal").className= ""
+    setTimeout(function() {
+      document.getElementById("modal").className = "hidden"} ,3000);
+  })
+  }
+    )
+}
 
 
 
